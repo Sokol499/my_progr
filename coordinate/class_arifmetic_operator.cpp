74b664f2a0bf354886c6dbd979cfab5579113fe2
Sokol499 <sokolovita2005@gmail.com>
2021-04-16 16:09:24 +0300

    coordinate

---
+++ coordinate/class_arifmetic_operator.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+using namespace std;
+
+class Coordinate
+{
+private:
+    int x;
+    int y;
+    int z;
+public:
+
+    Coordinate(int x_, int y_, int z) : x(x_), y(y_), z(z)
+    {
+    }
+
+    ~Coordinate()
+    {
+    }
+
+    void Print() const
+    {
+        cout << "Coordinate(x, y, z) = (" << x << ", " << y << ", " << z << ")" << endl;
+    }
+
+    bool operator==(const Coordinate& other) const
+    {
+        return x == other.x && y == other.y && z == other.z;
+    }
+
+    bool operator<=(const Coordinate& other) const
+    {
+        int len1 = x * x + y * y + z * z;
+        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
+        return len1 < len2;
+    }
+
+    bool operator>=(const Coordinate& other) const
+    {
+        int len1 = x * x + y * y + z * z;
+        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
+        return len1 > len2;
+    }
+
+    bool operator>(const Coordinate& other) const
+    {
+        int len1 = x * x + y * y + z * z;
+        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
+        return len1 > len2;
+    }
+
+    bool operator<(const Coordinate& other) const
+    {
+        int len1 = x * x + y * y + z * z;
+        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
+        return len1 < len2;
+    }
+
+    Coordinate operator+=(const Coordinate& other)
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+    
+    Coordinate operator-=(const Coordinate& other)
+    {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
+
+    Coordinate operator*=(const Coordinate& other)
+    {
+        x *= other.x;
+        y *= other.y;
+        z *= other.z;
+        return *this;
+    }
+
+    Coordinate operator/=(const Coordinate& other)
+    {
+        x /= other.x;
+        y /= other.y;
+        z /= other.z;
+        return *this;
+    }
+
+    Coordinate operator%=(const Coordinate& other)
+    {
+        x %= other.x;
+        y %= other.y;
+        z %= other.z;
+        return *this;
+    }
+};
+
+
+int main()
+{
+    return 0;
+}
