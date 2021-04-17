dc5291aaca233de4bcad4eacc4cfe3ff91ba0888
Sokol499 <sokolovita2005@gmail.com>
2021-04-17 12:40:23 +0300

    error correction

--- coordinate/class_arifmetic_operator.cpp
+++ coordinate/class_arifmetic_operator.cpp
@@ -4,94 +4,90 @@ using namespace std;
 class Coordinate
 {
 private:
-    int x;
-    int y;
-    int z;
+    int x_;
+    int y_;
+    int z_;
 public:
 
-    Coordinate(int x_, int y_, int z) : x(x_), y(y_), z(z)
-    {
-    }
+    Coordinate(int x, int y, int z) : x_(x), y_(y), z_(z) {}
 
-    ~Coordinate()
-    {
-    }
+    ~Coordinate(){}
 
     void Print() const
     {
-        cout << "Coordinate(x, y, z) = (" << x << ", " << y << ", " << z << ")" << endl;
+        cout << "Coordinate(x, y, z) = (" << x_ << ", " << y_ << ", " << z_ << ")" << endl;
     }
 
     bool operator==(const Coordinate& other) const
     {
-        return x == other.x && y == other.y && z == other.z;
+        return x_ == other.x_ && y_ == other.y_ && z_ == other.z_;
     }
 
     bool operator<=(const Coordinate& other) const
     {
-        int len1 = x * x + y * y + z * z;
-        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
-        return len1 < len2;
+        int len1 = x_ * x_ + y_ * y_ + z_ * z_;
+        int len2 = other.x_ * other.x_ + other.y_ * other.y_ + other.z_ * other.z_;
+        return len1 <= len2;
     }
 
     bool operator>=(const Coordinate& other) const
     {
-        int len1 = x * x + y * y + z * z;
-        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
-        return len1 > len2;
+        int len1 = x_ * x_ + y_ * y_ + z_ * z_;
+        int len2 = other.x_ * other.x_ + other.y_ * other.y_ + other.z_ * other.z_;
+        return len1 >= len2;
     }
 
     bool operator>(const Coordinate& other) const
     {
-        int len1 = x * x + y * y + z * z;
-        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
+        int len1 = x_ * x_ + y_ * y_ + z_ * z_;
+        int len2 = other.x_ * other.x_ + other.y_ * other.y_ + other.z_ * other.z_;
         return len1 > len2;
     }
 
     bool operator<(const Coordinate& other) const
     {
-        int len1 = x * x + y * y + z * z;
-        int len2 = other.x * other.x + other.y * other.y + other.z * other.z;
+        int len1 = x_ * x_ + y_ * y_ + z_ * z_;
+        int len2 = other.x_ * other.x_ + other.y_ * other.y_ + other.z_ * other.z_;
         return len1 < len2;
     }
 
     Coordinate operator+=(const Coordinate& other)
     {
-        x += other.x;
-        y += other.y;
-        z += other.z;
+        x_ += other.x_;
+        y_ += other.y_;
+        z_ += other.z_;
         return *this;
     }
     
     Coordinate operator-=(const Coordinate& other)
     {
-        x -= other.x;
-        y -= other.y;
-        z -= other.z;
+        x_ -= other.x_;
+        y_ -= other.y_;
+        z_ -= other.z_;
         return *this;
     }
 
     Coordinate operator*=(const Coordinate& other)
     {
-        x *= other.x;
-        y *= other.y;
-        z *= other.z;
+        x_ *= other.x_;
+        y_ *= other.y_;
+        z_ *= other.z_;
         return *this;
     }
 
     Coordinate operator/=(const Coordinate& other)
     {
-        x /= other.x;
-        y /= other.y;
-        z /= other.z;
+        x_ /= other.x_;
+        y_ /= other.y_;
+        z_ /= other.z_;
         return *this;
     }
 
     Coordinate operator%=(const Coordinate& other)
     {
-        x %= other.x;
-        y %= other.y;
-        z %= other.z;
+        x_ %= other.x_;
+        y_ %= other.y_;
+        z_ %= other.z_;
         return *this;
     }
 };
