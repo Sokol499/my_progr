6d2cdd2a0bb1cf015f87fed1747834b340fc8112
Sokol499 <sokolovita2005@gmail.com>
2021-04-19 17:34:02 +0300

    error correction

--- merge_sort/Merge_Sort.cpp
+++ merge_sort/Merge_Sort.cpp
@@ -1,58 +1,45 @@
 ﻿#include <iostream>
 using namespace std;
-
-void Merge(int* arr, int left, int right) //функция , которая сливает массив
+template <class T>
+void Merge(T* arr, int left, int right) // функция , которая сливает массив
 {
-    int middle , start , final , j;
-    int* array = new int[100];
-    middle = (left + right) / 2; //вычисление среднего элемента
-    start = left; //начало левой части
-    final = middle + 1; //начало правой части
-    for (j = left; j <= right; j++) //выполнять от начала до конца
+    int middle , start , finish , j;
+    int* array = new T[right - left + 1];
+    middle = (left + right) / 2; // вычисление среднего элемента
+    start = left; // начало левой части
+    finish = middle + 1; // начало правой части
+    for (j = left; j <= right; j++) // выполнять от начала до конца
     {
-        if ((start <= middle) && ((final > right) || (arr[start] < arr[final])))
+        if ((start <= middle) && ((finish > right) || (arr[start] < arr[finish])))
         {
             array[j] = arr[start];
-            start++;
+            ++start;
         }
         else
         {
-            array[j] = arr[final];
-            final++;
+            array[j] = arr[finish];
+            ++finish;
         }
     }
-    for (j = left; j <= right; j++) //возврат результата
+    for (j = left; j <= right; j++) // возврат результата
     {
         arr[j] = array[j];
     }
     delete[] array;
 };
-void MergeSort(int* arr, int left, int right) //рекурсивная функция сортировки
+
+template <class T>
+void MergeSort(T* arr, T left, T right) // рекурсивная функция сортировки
 {
     if (left < right)
     {
-        MergeSort(arr, left, (left + right) / 2); //сортировка левой части
-        MergeSort(arr, (left + right) / 2 + 1, right);  //сортировка правой части
-        Merge(arr, left, right);  //слияние двух частей
+        MergeSort(arr, left, (left + right) / 2); // сортировка левой части
+        MergeSort(arr, (left + right) / 2 + 1, right);  // сортировка правой части
+        Merge(arr, left, right);  // слияние двух частей
     }
 }
-void main() //главная функция
+
+int main() // главная функция
 {
-    setlocale(LC_ALL, "Rus");
-    int i, n;
-    int* arr = new int[100];
-    cout << "Размер массива :";
-    cin >> n;
-    for (i = 1; i <= n; i++)
-    {
-        cout << i << " Элемент массива : ";
-        cin >> arr[i];
-    }
-    MergeSort(arr, 1, n); //вызов сортировки
-    cout << "Упорядоченный массив по возрастанию : "; //вывод упорядоченного массива
-    for (i = 1; i <= n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    delete[] arr;
+    return 0;
 };
