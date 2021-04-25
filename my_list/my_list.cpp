083211195c8fa98c7c3415de6e3d823522aacedf
Sokol499 <sokolovita2005@gmail.com>
2021-04-25 11:44:40 +0300

    list correction

--- my_list/my_list.cpp
+++ my_list/my_list.cpp
@@ -1,122 +1,151 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
 template <class T>
 class List
 {
 private:
-	struct Node
-	{
-		Node* prev_p;
-		Node* next_p;
-		T element;
-		Node(const T& elem) : element(elem)
-		{
-			next_p = prev_p = nullptr;
-		}
-	};
-	Node* start_;
-	Node* end_;
-	int size_;
+    struct Node
+    {
+        Node* prev_p;
+        Node* next_p;
+        T element;
+        Node(const T& elem) : element(elem)
+        {
+            next_p = prev_p = nullptr;
+        }
+    };
+    Node* start_;
+    Node* end_;
+    int size_;
 public:
-	List()
-	{
-		end_ = start_ = nullptr;
-		size_ = 0;
-	}
+    List()
+    {
+        end_ = start_ = nullptr;
+        size_ = 0;
+    }
 
-	List(const T& elem)
-	{
-		start_ = end_ = new Node(elem);
-		size_ = 1;
-	}
+    List(const T& elem)
+    {
+        start_ = end_ = new Node(elem);
+        size_ = 1;
+    }
 
-	int size() const
-	{
-		return size_;
-	}
+    List(T& vec)
+    {
+        vec.push_back(vector <T>());
+    }
 
-	void push_back(const T& elem)
-	{
-		if (end_ == nullptr)
-		{
-			return;
-		}
-		Node* current = new Node(elem)
-		end_->next_p = current;
-		current->prev_p = end_;
-		end_ = current;
-		++size_;
-	}
+    bool isEmpty()
+    {
+        if (List.isEmpty())
+        {
+            cout << "True";
+        }
+        else
+        {
+            cout << "False";
+        }
+    }
 
-	void push_start(const T& elem)
-	{
-		if (start_ == nullptr)
-		{
-			return;
-		}
-		Node* current = new Node(elem)
-		start_->prev_p = current;
-		current->next_p = start_;
-		start_ = current;
-		++size_;
-	}
+    int size() const
+    {
+        return size_;
+    }
 
-	T pop_back()
-	{
-		if (size_ <= 1)
-		{
-			if (size_ == 0)
-			{
-				return T();
-			}
-			T current = start_->element;
-			delete start_;
-			start_ = end_ = nullptr;
-			size_ = 0;
-			return current;
-		}
-		T current = end_->element;
-		Node* before_end = end_->prev_p;
-		before_end->next_p = nullptr;
-		delete end_;
-		end_ = before_end;
-		--size_;
-		return current;
-	}
+    void push_back(const T& elem)
+    {
+        if (start_ == nullptr)
+        {
+            start_ = end_ = new Node(elem);
+            size_ = 1;
+            return;
+        }
+        Node* current = new Node(elem);
+        end_->next_p = current;
+        current->prev_p = end_;
+        end_ = current;
+        ++size_;
+    }
 
-	T pop_start()
-	{
-		if (size_ <= 1)
-		{
-			if (size_ == 0)
-			{
-				return T();
-			}
-			T current = start_->element;
-			delete start_;
-			start_ = end_ = nullptr;
-			size_ = 0;
-			return current;
-		}
-		T current = start_->element;
-		Node* after_start = start_->next_p;
-		after_start->prev_p = nullptr;
-		delete start_;
-		start_ = after_start;
-		--size_;
-		return current;
-	}
+    void push_start(const T& elem)
+    {
+        if (start_ == nullptr)
+        {
+            start_ = end_ = new Node(elem);
+            size_ = 1;
+            return;
+        }
+        Node* current = new Node(elem);
+        start_->prev_p = current;
+        current->next_p = start_;
+        start_ = current;
+        ++size_;
+    }
 
-	~List()
-	{
-		while (start_ != nullptr)
-		{
-			Node* next = start_->next_p;
-			delete start_;
-			start_ = next
-		}
-		start_ = end_ = nullptr;
-		size_ = 0;
-	}
+    T pop_back()
+    {
+        if (size_ <= 1)
+        {
+            if (size_ == 0)
+            {
+                return T();
+            }
+            T current = start_->element;
+            delete start_;
+            start_ = end_ = nullptr;
+            size_ = 0;
+            return current;
+        }
+        T current = end_->element;
+        Node* before_end = end_->prev_p;
+        before_end->next_p = nullptr;
+        delete end_;
+        end_ = before_end;
+        --size_;
+        return current;
+    }
+
+    T pop_start()
+    {
+        if (size_ <= 1)
+        {
+            if (size_ == 0)
+            {
+                return T();
+            }
+            T current = start_->element;
+            delete start_;
+            start_ = end_ = nullptr;
+            size_ = 0;
+            return current;
+        }
+        T current = start_->element;
+        Node* after_start = start_->next_p;
+        after_start->prev_p = nullptr;
+        delete start_;
+        start_ = after_start;
+        --size_;
+        return current;
+    }
+
+    ~List()
+    {
+        while (start_ != nullptr)
+        {
+            Node* next = start_->next_p;
+            delete start_;
+            start_ = next;
+        }
+        start_ = end_ = nullptr;
+        size_ = 0;
+    }
 };
+
+int main()
+{
+    List<int> list;
+    std::cout << "hello" << endl;
+    return 0;
+}
