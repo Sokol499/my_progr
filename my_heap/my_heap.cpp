d57a2ce931dc3f13ff5258504a226554f9b98db1
Sokol499 <sokolovita2005@gmail.com>
2021-04-26 10:58:13 +0300

    heap correction

--- my_heap/my_heap.cpp
+++ my_heap/my_heap.cpp
@@ -9,46 +9,44 @@ class Heap
 private:
     int size_;
     std::vector<T> arr_;
-    void ShiftUp(T* arr_, int k)
+    void ShiftUp(int k)
     {
         while (true)
         {
-            int parent = k - 1 / 2;
+            int parent = (k - 1) / 2;
+            if (k == 0)
             {
-                if (k == 0)
-                {
-                    return;
-                }
-                if (arr_[parent] < arr_[k])
-                {
-                    swap(arr_[parent], arr_[k]);
-                    k = parent;
-                }
-                else
-                {
-                    return;
-                }
+                return;
+            }
+            if (arr_[parent] < arr_[k])
+            {
+                std::swap(arr_[parent], arr_[k]);
+                k = parent;
+            }
+            else
+            {
+                return;
             }
         }
     }
-    void ShiftDown(T*arr_, int k)
+    void ShiftDown(int k)
     {
         while (true)
         {
             int first_child = k * 2 + 1;
-            int second_child = k * 2 + 1;
+            int second_child = k * 2 + 2;
             if (first_child < arr_.size())
             {
                 if (arr_[first_child] > arr_[k])
                 {
-                    swap(arr_[first_child], arr_[k]);
+                    std::swap(arr_[first_child], arr_[k]);
                     k = first_child;
                 }
-                else
+                if (second_child < arr_.size())
                 {
                     if (arr_[second_child] > arr_[k])
                     {
-                        swap(arr_[second_child], arr_[k]);
+                        std::swap(arr_[second_child], arr_[k]);
                         k = second_child;
                     }
                     else
@@ -64,7 +62,7 @@ private:
         }
     }
 public:
-    void Add(T*arr_, const T& elem)
+    void Add(const T& elem)
     {
         arr_.push_back(elem);
         ShiftUp(arr_.size() - 1);
@@ -74,17 +72,17 @@ public:
     {
         Add(elem);
     }
-    Heap(const vector<T>& vec)
+    Heap(const std::vector<T>& vec)
     {
         for (int i = 0; i < vec.size(); ++i)
         {
             Add(vec[i]);
         }
     }
-    T PopMax(T* arr_)
+    T PopMax()
     {
         T maximum = arr_[0];
-        swap(arr_[0], arr_[arr_.size() - 1]);
+        std::swap(arr_[0], arr_[arr_.size() - 1]);
         arr_.pop_back();
         ShiftDown(0);
         return maximum;
@@ -93,7 +91,6 @@ public:
 
 int main()
 {
-    cout << "Hello Ivan!\n";
     return 0;
 }
 
