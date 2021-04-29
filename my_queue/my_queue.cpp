43fc2bb56d2b7bb96be27e008b9867370bbdc27a
Sokol499 <sokolovita2005@gmail.com>
2021-04-29 14:13:29 +0300

    queue correction2

--- my_queue/my_queue.cpp
+++ my_queue/my_queue.cpp
@@ -2,63 +2,88 @@
 #include <stack>
 using namespace std;
 
-template <class T>
 class Queue
 {
 private:
-	stack<T> s1_; // первый стек
-	stack<T> s2_; // второй стек
-	int size_;
-	int elem_;
-	T* buffer_;
+    stack <int> s1_;
+    stack <int> s2_;
 public:
-	Queue()
-	{
-		buffer_ = new T[1];
-		elem_ = 0;
-		size_ = 1;
-	}
+    void push(int k)
+    {
+        s1_.push(k);
+    }
+    void pop()
+    {
+        if (s2_.empty())
+        {
+            while (!s1_.empty())
+            {
+                s2_.push(s1_.top());
+                s1_.pop();
+            }
+            s2_.pop();
+        }
+    };
 
-	Queue(const Queue<T>& other)
-	{
-		size_ = other.size_;
-		elem_ = other.elem_;
-		buffer_ = new T[size_];
-		for (int i = 0; i < elem_; ++i)
-		{
-			buffer_[i] = other.buffer_[i];
-		}
-	}
-
-	void push(T k) // кладем элементы в стек s1
-	{
-		s1_.push(k);
-	}
-
-	void pop() // для каждого элемента делаем не более одного перекладывания из стека в стек
-	{
-		if (s2_.empty)
-		{
-			while (!s1_.empty())
-			{
-				T elem_ = s1_.top();
-				s1_.pop();
-				s2_.push(elem_);
-			}
-		}
-		s2_.top();
-		s2_.pop();
-		return elem_;
-	}
-	
-	~Queue()
-	{
-		delete[] buffer_;
-	}
+    void print()
+    {
+        while (!s1_.empty())
+        {
+            s2_.push(s1_.top());
+            s1_.pop();
+        }
+        //вывод элементов
+        while (!s2_.empty())
+        {
+            cout << s2_.top() << ' ';
+            s1_.push(s2_.top());
+            s2_.pop();
+        }
+    }
 };
 
 int main()
 {
-	return 0;
-}
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
 
+    q.print();
+    cout << endl;
+
+    //удаляем элемент очереди
+    q.pop();
+    q.print();
+    cout << endl;
+
+    //удаляем элемент очереди
+    q.pop();
+    q.print();
+    cout << endl;
+
+    //удаляем элемент очереди
+    q.pop();
+    q.print();
+    cout << endl;
+
+    //удаляем элемент очереди
+    q.pop();
+    q.print();
+    cout << endl;
+
+    //удаляем элемент очереди
+    q.pop();
+    q.print();
+    cout << endl;
+
+    //удаляем элемент очереди
+    q.pop();
+    q.print();
+    cout << endl;
+
+    return 0;
+}
