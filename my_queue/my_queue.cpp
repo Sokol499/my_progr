88567c239f0c708cb0faaa3513fc5c93acd06241
Sokol499 <sokolovita2005@gmail.com>
2021-04-29 12:58:01 +0300

    queue correction1

--- my_queue/my_queue.cpp
+++ my_queue/my_queue.cpp
@@ -3,30 +3,57 @@
 using namespace std;
 
 template <class T>
-class queue
+class Queue
 {
-	stack <T> s1_; // первый стек
-	stack <T> s2_; // второй стек
+private:
+	stack<T> s1_; // первый стек
+	stack<T> s2_; // второй стек
+	int size_;
+	int elem_;
+	T* buffer_;
 public:
-	void push(T n) // кладем элементы в стек s1
+	Queue()
 	{
-		s1_.push(n);
+		buffer_ = new T[1];
+		elem_ = 0;
+		size_ = 1;
+	}
+
+	Queue(const Queue<T>& other)
+	{
+		size_ = other.size_;
+		elem_ = other.elem_;
+		buffer_ = new T[size_];
+		for (int i = 0; i < elem_; ++i)
+		{
+			buffer_[i] = other.buffer_[i];
+		}
+	}
+
+	void push(T k) // кладем элементы в стек s1
+	{
+		s1_.push(k);
 	}
 
 	void pop() // для каждого элемента делаем не более одного перекладывания из стека в стек
 	{
-		while (!s1_.empty())
+		if (s2_.empty)
 		{
-			s2_.push(s1_.top());
-			s1_.pop();
+			while (!s1_.empty())
+			{
+				T elem_ = s1_.top();
+				s1_.pop();
+				s2_.push(elem_);
+			}
 		}
+		s2_.top();
 		s2_.pop();
-		//добавляем элементы обратно
-		while (!s2_.empty())
-		{
-			s1_.push(s2_.top());
-			s2_.pop();
-		}
+		return elem_;
+	}
+	
+	~Queue()
+	{
+		delete[] buffer_;
 	}
 };
 
